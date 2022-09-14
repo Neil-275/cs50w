d558fb6b42c3f6e036ceddd4bd98028857ad2d48
unknown <onghoangcodebug01@gmail.com>
2022-09-14 17:31:24 +0700

    add a cpp file

---
+++ hello.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define el cout<<'\n'
+#define pii pair <int,int>
+#define ll long long
+#define prob ""
+using namespace std;
+
+const int N=1e5,mod=1e9+7;
+
+void input()
+{
+    cout<<"HELLO WORLD!!!!!";
+    cout<<"HELLO WORLD!!!!!";
+    cout<<"HELLO WORLD!!!!!";el;
+    for (int i=1;i<=10;i++) cout<<i<<' ';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+//    freopen(prob".inp","a+",stdin);
+//    freopen(prob".out","w+",stdout);
+//    int t;
+//    cin>>t;
+//    while (t--)
+
+    input();
+    return 0;
+}
+
